ad6732c597c4ade2c5bbd2473d4f6270895d4d11
2099pritesh-hub <2099pritesh@gmail.com>
2026-04-25 09:26:58 +0530

    Add: number-of-students-unable-to-eat-lunch - submission-3

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        q = deque(students)
+        res = len(students)
+
+        for sandwich in sandwiches:
+            cnt = 0
+            while cnt < len(q) and q[0] != sandwich:
+                q.append(q.popleft())
+                cnt += 1
+            
+            if q[0] == sandwich:
+                q.popleft()
+                res -= 1
+            else:
+                break
+        
+        return res
