e1147ca5968e86a00b98e0d49851e99e7630d280
2099pritesh-hub <2099pritesh@gmail.com>
2026-06-23 11:42:25 +0530

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = {}
+
+        for i in range(numCourses):
+            adjList[i] = []
+
+        for src, dst in prerequisites:
+            adjList[src].append(dst)
+        
+        def dfs(n, visit):
+            if n in visit:
+                return False
+            
+            
+            visit.add(n)
+            for neighbor in adjList[n]:
+                if not dfs(neighbor, visit):
+                    return False
+            visit.remove(n)
+            adjList[n] = []
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i, set()):
+                return False
+        return True
