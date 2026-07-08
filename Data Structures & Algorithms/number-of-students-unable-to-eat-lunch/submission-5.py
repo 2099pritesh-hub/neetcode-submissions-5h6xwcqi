0a99086994f5ea7121c32a14ed0d7ab58432fef0
2099pritesh-hub <2099pritesh@gmail.com>
2026-07-08 18:09:47 +0530

    Add: number-of-students-unable-to-eat-lunch - submission-5

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        hashMap = {0:0, 1:0}
+        for s in students:
+            hashMap[s] = hashMap.get(s, 0) + 1
+        
+        res = len(students)
+        for s in sandwiches:
+            if hashMap[s] > 0:
+                res -= 1
+                hashMap[s] -= 1
+            else:
+                return res
+        return res
