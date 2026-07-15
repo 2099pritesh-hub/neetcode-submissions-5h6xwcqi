7d9f7575452cefc84021ecd1bcf26415b2ce38c7
2099pritesh-hub <2099pritesh@gmail.com>
2026-07-16 00:16:39 +0530

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.py
@@ -0,0 +1,43 @@
+class ListNode:
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.left, self.right = ListNode(), ListNode()
+        self.left.next, self.right.prev = self.right, self.left
+        self.capacity = capacity
+        self.cache = {}
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+    
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+        node.prev = node.next = None
+    
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        node.prev, node.next = prev, nxt
+        prev.next = nxt.prev = node
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key].val = value
+            self.remove(self.cache[key])
+        else:
+            self.cache[key] = ListNode(key, value)
+        self.insert(self.cache[key])
+        
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
