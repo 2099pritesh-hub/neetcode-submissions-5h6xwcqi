ab8df8b38abdc939703ec234585df4fe353ee8fd
2099pritesh-hub <2099pritesh@gmail.com>
2026-07-19 09:24:05 +0530

    Add: unique-paths-ii - submission-12

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-12.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        cache = {}
+    
+        def dfs(r, c):
+            if r == m or c == n or obstacleGrid[r][c] == 1:
+                return 0
+            if r == m - 1 and c == n - 1:
+                return 1
+            if (r, c) in cache:
+                return cache[(r, c)]
+                
+            cache[(r, c)] =  dfs(r + 1, c) + dfs(r, c + 1)
+            return cache[(r, c)]
+
+        return dfs(0, 0)
