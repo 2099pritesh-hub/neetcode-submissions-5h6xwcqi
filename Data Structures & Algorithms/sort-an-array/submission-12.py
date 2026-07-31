e6d2aeea72b4c77bacb2d86d373da26944e766e6
2099pritesh-hub <2099pritesh@gmail.com>
2026-07-31 19:07:47 +0530

    Add: sort-an-array - submission-12

---
+++ Data Structures & Algorithms/sort-an-array/submission-12.py
@@ -0,0 +1,39 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def divide(s, e):
+            if s >= e:
+                return
+            
+            m = (s + e) // 2
+            divide(s, m)
+            divide(m + 1, e)
+            merge(s, m , e)
+    
+        def merge(s, m, e):
+            left = nums[s: m + 1]
+            right = nums[m + 1: e + 1]
+
+            i = 0
+            j = 0
+            k = s
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    nums[k] = left[i]
+                    i += 1
+                else:
+                    nums[k] = right[j]
+                    j += 1
+                k += 1
+            
+            while i < len(left):
+                nums[k] = left[i]
+                i += 1
+                k += 1
+            while j < len(right):
+                nums[k] = right[j]
+                j += 1
+                k += 1
+        
+        divide(0, len(nums) - 1)
+        return nums
