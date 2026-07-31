6ce519d876d3768852a722f3fb2c8905e97e9aac
2099pritesh-hub <2099pritesh@gmail.com>
2026-07-31 08:04:51 +0530

    Add: sort-an-array - submission-9

---
+++ Data Structures & Algorithms/sort-an-array/submission-9.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.maxHeapLen = len(nums)
+        self.heapify(nums)
+        while self.maxHeapLen > 0:
+            self.pop(nums)
+        return nums
+
+    def heapify(self, arr):
+        cur = (self.maxHeapLen // 2) - 1
+
+        while cur >= 0:
+            i = cur
+            self.percolateDown(i, arr)
+            cur -= 1
+    
+    def pop(self, arr):
+        arr[0], arr[self.maxHeapLen - 1] = arr[self.maxHeapLen - 1], arr[0]
+        self.maxHeapLen -= 1
+        i = 0
+        self.percolateDown(i, arr)
+
+    def percolateDown(self, i, arr):
+        while 2 * i + 1 < self.maxHeapLen:
+            if (2 * i + 2 < self.maxHeapLen and
+                arr[2 * i + 2] > arr[2 * i + 1] and
+                arr[i] < arr[2 * i + 2]):
+                arr[i], arr[2 * i + 2] = arr[2 * i + 2], arr[i]
+                i = 2 * i + 2
+            elif arr[i] < arr[2 * i + 1]:
+                arr[i], arr[2 * i + 1] = arr[2 * i + 1], arr[i]
+                i = 2 * i + 1
+            else:
+                break
