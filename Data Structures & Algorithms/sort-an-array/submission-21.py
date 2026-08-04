97e0288ad090d3678f3644cb617a5127dc0b627b
2099pritesh-hub <2099pritesh@gmail.com>
2026-08-04 12:52:09 +0530

    Add: sort-an-array - submission-21

---
+++ Data Structures & Algorithms/sort-an-array/submission-21.py
@@ -0,0 +1,25 @@
+import random
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def quickSort(l, r):
+            if l >= r:
+                return
+            
+            pivotIndex = random.randint(l, r)
+            nums[pivotIndex], nums[r] = nums[r], nums[pivotIndex]
+
+            pivot = nums[r]
+            i = l
+
+            for j in range(l, r):
+                if nums[j] <= pivot:
+                    nums[j], nums[i] = nums[i], nums[j]
+                    i += 1
+            
+            nums[i], nums[r] = nums[r], nums[i]
+
+            quickSort(l, i - 1)
+            quickSort(i + 1, r)
+        
+        quickSort(0, len(nums) - 1)
+        return nums
