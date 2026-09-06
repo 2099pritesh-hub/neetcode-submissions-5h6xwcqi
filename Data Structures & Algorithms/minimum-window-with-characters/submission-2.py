73a769560cd10dc8eb55b0844f4c7749a8f6b1f3
2099pritesh-hub <2099pritesh@gmail.com>
2026-09-06 18:11:02 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        countT, countS = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], len(s) + 1
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            countS[c] = 1 + countS.get(c, 0)
+            
+            if c in countT and countS[c] == countT[c]:
+                have += 1
+            
+            while have == need:
+                if (r - l + 1) < resLen:
+                    resLen = (r - l + 1)
+                    res = [l, r]
+                countS[s[l]] -= 1
+                if s[l] in countT and countS[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        
+        l, r = res
+        return s[l: r + 1] if resLen != len(s) + 1 else ""
